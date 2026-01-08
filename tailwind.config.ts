4c35373de832da67369780efca25b5b66dce7e75
Samuel Gonzalez Alberto <seg42@njit.edu>
2026-01-07 20:24:56 -0500

    feat: configure swiss grid tailwind tokens

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        swiss: 'repeat(12, minmax(0, 1fr))',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
